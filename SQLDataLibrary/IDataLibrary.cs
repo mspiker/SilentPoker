3349091267cbfc1dd5d2e8d245dc25bfd8813913
Mark Spiker <kb8uvv@gmail.com>
2024-03-18 16:43:57 -0400

    Implemented SQL Data Library.

---
+++ SQLDataLibrary/IDataLibrary.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataLibrary
+{
+    public interface IDataLibrary
+    {
+        Task<int> Execute<T>(string sql, T parameters, string connectionString);
+        Task<T?> GetRecord<T, U>(string sql, U parameters, string connectionString);
+        Task<List<T>> GetRecords<T, U>(string sql, U parameters, string connectionString);
+    }
+}
---
+++ SQLDataLibrary/SQLDataLibrary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataLibrary
+{
+    public class SQLDataLibrary : IDataLibrary
+    {
+        public async Task<List<T>> GetRecords<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+                return data.ToList();
+            }
+        }
+        public async Task<T?> GetRecord<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryFirstOrDefaultAsync<T>(sql, parameters);
+                return data;
+            }
+        }
+        public async Task<int> Execute<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                int x = await connection.ExecuteAsync(sql, parameters);
+                return x;
+            }
+        }
+    }
+}
