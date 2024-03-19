cd309063835b667db4359ee7b9dc64265bfff0ee
Mark Spiker <kb8uvv@gmail.com>
2024-03-19 07:26:00 -0400

    Cleaned up calls to end points so they will be easier to build.

---
+++ ServiceNowAPI/EndpointLibrary.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace EndpointAPI
+{
+    public class EndpointLibrary
+    {
+        private readonly HttpClient _httpClient;
+
+        public EndpointLibrary(string BaseAddress, string AuthorizationHeader)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(BaseAddress);
+            _httpClient.DefaultRequestHeaders.Add("Authorization", AuthorizationHeader);
+        }
+        public string Endpoint(string table, string query, string fields)
+        {
+            string endpoint = $"/api/now/table/{table}?sysparm_query={query}&sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields={fields}";
+            return endpoint;
+        }
+        public async Task<List<T>?> CallAPI<T>(string endpoint)
+        {
+            var response = await _httpClient.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                JObject root = JObject.Parse(content);
+                if (root != null)
+                    if (root.ContainsKey("result"))
+                        return root["result"].ToObject<List<T>>();
+                return null;
+            }
+            else
+            {
+                // Handle error responses here
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return null;
+            }
+        }
+    }
+}
--- SilentPoker/Services/ServiceNowAPI.cs
+++ SilentPoker/Services/ServiceNowAPI.cs
@@ -1,45 +1,22 @@
-﻿using Newtonsoft.Json.Linq;
-using SilentPoker.Models;
+﻿using SilentPoker.Models;
+using EndpointAPI;
 
 namespace SilentPoker.Services
 {
     public class ServiceNowAPI
     {
-        private readonly HttpClient _httpClient;
+        private readonly EndpointLibrary _endpointLibrary;
 
         public ServiceNowAPI(string BaseAddress, string AuthorizationHeader)
         {
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri(BaseAddress);
-            _httpClient.DefaultRequestHeaders.Add("Authorization", AuthorizationHeader);
-        }
-
-        public string Endpoint(string table, string query, string fields)
-        {
-            string endpoint = $"/api/now/table/{table}?sysparm_query={query}&sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields={fields}";
-            return endpoint;
+            _endpointLibrary = new EndpointLibrary(BaseAddress, AuthorizationHeader);
         }
 
         public async Task<List<Sprint>?> GetSprints()
         {
-            var response = await _httpClient.GetAsync(
-                Endpoint("rm_sprint", "active=true", "number%2Cshort_description%2Csys_id"));
-            response.EnsureSuccessStatusCode();
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                JObject root = JObject.Parse(content);
-                if (root != null)
-                    if (root.ContainsKey("result"))
-                        return root["result"].ToObject<List<Sprint>>();
-                return null;
-            }
-            else
-            {
-                // Handle error responses here
-                Console.WriteLine($"Error: {response.StatusCode}");
-                return null;
-            }
+            var result = await _endpointLibrary.CallAPI<Sprint?>(
+                _endpointLibrary.Endpoint("rm_sprint", "active=true", "number%2Cshort_description%2Csys_id"));
+            return result;
         }
 
     }
