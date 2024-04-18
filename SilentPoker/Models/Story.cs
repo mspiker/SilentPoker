a2746287468723a52bba42a812239c48e8d5764c
Mark Spiker <kb8uvv@gmail.com>
2024-04-17 21:03:00 -0400

    Changed some of the calculations for variance, average and participation to calculate at the class level.

--- SilentPoker/Models/Story.cs
+++ SilentPoker/Models/Story.cs
@@ -11,5 +11,50 @@
         public int MyVote { get; set; }
         public string? Priority { get; set; }
         public List<Vote>? Votes { get; set; }
+        public double VoteAverage { get; set; }
+        public double VoteVariance { get; set; }
+        public double VoteParticipation { get; set; }
+
+        public void CaclulateVote(List<Member> roomMembers)
+        {
+            // Determine qualifying votes
+            int qualifyingMembers = 0;
+            int votingMembers = 0;
+            int sumOfVotes = 0;
+            foreach (var member in roomMembers)
+            {
+                if (member.Role == MemberRole.Developer || member.Role == MemberRole.ScrumMaster)
+                {
+                    qualifyingMembers++;
+                    if (Votes.Any(v => v.UserId == member.UserId))
+                    {
+                        votingMembers++;
+                        sumOfVotes += Votes.First(v => v.UserId == member.UserId).VoteValue;
+                    }
+                }
+            }
+
+            if (Votes == null || Votes.Count == 0 || qualifyingMembers == 0)
+            {
+                
+            } else
+            {
+                if (votingMembers == 0)
+                {
+                    this.VoteAverage = 0;
+                    this.VoteParticipation = 0;
+                    this.VoteVariance = 0;
+                }
+                else
+                {
+                    this.VoteAverage = sumOfVotes / votingMembers;
+                    this.VoteParticipation = (votingMembers * 1.0) / (qualifyingMembers * 1.0);
+                    this.VoteVariance = Votes.Sum(v => Math.Pow(v.VoteValue - VoteAverage, 2));
+                }
+            }
+
+            
+        }
+
     }
 }
